4e5d5081dff1e1ef8337f778dcb9c6e84e1bac42
parshva555 <parshvadani@gmail.com>
2023-12-20 23:51:27 +0530

    Adding Copying to ClipBoard Functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,8 @@ const Demo = () => {
   const [allArticles,setAllArticles] = useState([]);
 
   const [getSummary,{error,isFetching}] = useLazyGetSummaryQuery();
+
+  const [copied ,setCopied] = useState("")
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
     if(articlesFromLocalStorage){
@@ -29,6 +31,12 @@ const Demo = () => {
       localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
      }
   }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false),3000);
+  }
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
@@ -43,7 +51,7 @@ const Demo = () => {
             {allArticles.map((item,index) => (
               <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
                 <div className='copy_btn'>
-                  <img src={copy} alt="copy_icon"  className='w-[40%] h-[40%] object-contain'/>
+                  <img src={copied === item.url ? tick : copy} alt="copy_icon" onClick={() => handleCopy(item.url)} className='w-[40%] h-[40%] object-contain'/>
                 </div>
                 <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
                   {item.url}
