afbeb6a7018b89ed4f7658f6f321a1c6f8969fe0
parshva555 <parshvadani@gmail.com>
2023-12-20 23:33:33 +0530

    Browsing URL History

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,8 +6,15 @@ const Demo = () => {
     url:'',
     summary:'',
   });
+  const [allArticles,setAllArticles] = useState([]);
 
   const [getSummary,{error,isFetching}] = useLazyGetSummaryQuery();
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage);
+    }
+  },[])
   const handleSubmit = async(e) =>{
     e.preventDefault();
      const { data } = await getSummary({
@@ -15,8 +22,11 @@ const Demo = () => {
      });
      if(data?.summary){
       const newArticle = { ...article, summary:data.summary};
+      const updatedAllArticles = [newArticle, ...allArticles]
       setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
       console.log(newArticle)
+      localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
      }
   }
   return (
@@ -29,6 +39,18 @@ const Demo = () => {
           <button type='submit' className='submit_btn' >S</button>
         </form>
         {/* Browse URL History */}
+        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+            {allArticles.map((item,index) => (
+              <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
+                <div className='copy_btn'>
+                  <img src={copy} alt="copy_icon"  className='w-[40%] h-[40%] object-contain'/>
+                </div>
+                <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                  {item.url}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
       {/* Display Results */}
     </section>
